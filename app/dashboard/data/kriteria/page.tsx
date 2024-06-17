2ff857abf398a39cb41ed7d948999ceee5c77834
Rizky FIrman Syah <rizfirsy@gmail.com>
2024-06-18 00:08:25 +0700

    tabel kriteria

--- app/dashboard/data/kriteria/page.tsx
+++ app/dashboard/data/kriteria/page.tsx
@@ -1,16 +1,68 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { kriteria, Kriteria } from "./data/column";
+import { DataTable } from "@/components/ui/data-table";
 
-const Kriteria = () => {
+async function getData(): Promise<Kriteria[]> {
+  return [
+    {
+      kode_kriteria: "C1",
+      nama_kriteria: "Kapasitas Pengguna",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 1,
+    },
+    {
+      kode_kriteria: "C2",
+      nama_kriteria: "Interaksi Visual",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 2,
+    },
+    {
+      kode_kriteria: "C2",
+      nama_kriteria: "Interaksi Visual",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 3,
+    },
+    {
+      kode_kriteria: "C2",
+      nama_kriteria: "Interaksi Visual",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 4,
+    },
+    {
+      kode_kriteria: "C2",
+      nama_kriteria: "Interaksi Visual",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 4,
+    },
+    {
+      kode_kriteria: "C2",
+      nama_kriteria: "Interaksi Visual",
+      bobot: "-",
+      jenis: "Benefit",
+      tingkat_prioritas: 3,
+    },
+  ];
+}
+
+const KriteriaScreen = async () => {
+  const data = await getData();
   return (
     <div>
       <div className="flex justify-between items-center">
-        <h1>Data Kriteria</h1>
+        <h1 className="text-3xl">Data Kriteria</h1>
         <Button>Tambah data</Button>
       </div>
-      <div>Table here</div>
+      <div className="mx-4 my-8">
+        <DataTable columns={kriteria} data={data} />
+      </div>
     </div>
   );
 };
 
-export default Kriteria;
+export default KriteriaScreen;
---
+++ components/ui/data-table.tsx
@@ -0,0 +1,118 @@
+"use client";
+import React from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+  SortingState,
+  getSortedRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import { DataTablePagination } from "./table-pagination";
+import FilterTable from "./filter-table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+
+    state: {
+      sorting,
+      columnFilters,
+    },
+  });
+
+  return (
+    <div>
+      <FilterTable
+        table={table}
+        columnName="nama_kriteria"
+        placeholder="Filter kriteria..."
+      />
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="text-center">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="text-center">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="py-4">
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  );
+}
