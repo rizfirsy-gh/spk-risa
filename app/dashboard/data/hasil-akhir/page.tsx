0b1a65a3ecf20357d01857f6155ed13ab0d897aa
Rizky FIrman Syah <rizfirsy@gmail.com>
2024-06-27 16:57:02 +0700

    Dinda hasil akhir

--- app/dashboard/data/hasil-akhir/page.tsx
+++ app/dashboard/data/hasil-akhir/page.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { kolomHasilAkhir, HasilAkhir } from "./kolom-hasil-akhir";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
 
-const HasilAkhir = () => {
-  return <div>HasilAkhir</div>;
+async function getData(): Promise<HasilAkhir[]> {
+  return [
+    {
+      nama_alternatif: "WhatsApp Group",
+      nilai: "3.300",
+      rangking: 1,
+    },
+  ];
+}
+
+const HasilAkhirScreen = async () => {
+  const data = await getData();
+  return (
+    <section>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl">Data Hasil Akhir</h1>
+        <Button>Cetak Data</Button>
+      </div>
+      <div className="mx-4 my-8">
+        <DataTable
+          columns={kolomHasilAkhir}
+          data={data}
+          columnName="nama_alternatif"
+          filterPlaceholder="Cari alternatif..."
+        />
+      </div>
+    </section>
+  );
 };
 
-export default HasilAkhir;
+export default HasilAkhirScreen;
